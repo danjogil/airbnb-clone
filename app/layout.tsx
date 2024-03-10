0f5c839f13c7384e17fa1b0d694c5ddde150201e
Daniel Gil <dannyjaygil@gmail.com>
2024-03-10 11:23:36 +0200

    Build reusable modal and button components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        {/* <Modal title="Hello World" isOpen /> */}
         <Navbar />
         {children}
       </body>
