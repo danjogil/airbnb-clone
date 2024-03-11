df6c85e14b68f3dbb47d8a45089cad148be42c00
Daniel Gil <dannyjaygil@gmail.com>
2024-03-11 14:35:33 +0200

    Set up cloudinary and build image step for uploading listings

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  // images: {
+  //   domains: [
+  //     "avatars.githubusercontent.com",
+  //     "lh3.googleusercontent.com",
+  //     "res.cloudinary.com",
+  //   ],
+  // },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "referrer-policy", value: "no-referrer" }],
+      },
+    ];
   },
 };
 
