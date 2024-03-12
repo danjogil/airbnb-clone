0dc90ed105139a9ad1d13a86caac21760555029d
Daniel Gil <dannyjaygil@gmail.com>
2024-03-12 10:37:49 +0200

    Build HeartButton component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return <EmptyState showReset />;
   }
 
@@ -23,7 +27,13 @@ export default function Home() {
       gap-8
     "
       >
-        <div>My future listings</div>
+        {listings.map((listing: any) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            currentUser={currentUser}
+          />
+        ))}
       </div>
     </Container>
   );
